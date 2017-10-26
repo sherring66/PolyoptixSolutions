6186a0ef955c0c9cd92816f10d8e304d8444596b
sherring66 <scott.herring66@gmail.com>
2017-10-26 16:40:01 -0700

    26-Oct 4:38 PM
    Camera saves to SD card after 3 seconds as stated in serial monitor, ultrasonics reading, acceleromter reading

--- ClassifiedPolyoptixCode/ClassifiedPolyoptixCode.ino
+++ ClassifiedPolyoptixCode/ClassifiedPolyoptixCode.ino
@@ -4,16 +4,19 @@
 #include <Adafruit_LSM303.h>
 #include <Adafruit_LSM303_U.h>
 
+//Camera library
+#include <Adafruit_VC0706.h>
+
 // include the SD library:
 #include <SPI.h>
 #include <SD.h>
 
 #define SONAR_NUM 3      // Number of sensors.
-#define MAX_DISTANCE 20 // Maximum distance (in cm) to ping.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
   NewPing(8, 9, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
-  NewPing(4, 5, MAX_DISTANCE),
+  NewPing(5, 4, MAX_DISTANCE),
   NewPing(6, 7, MAX_DISTANCE)
 };
 
@@ -32,12 +35,24 @@ SdFile root;
 // Adafruit SD shields and modules: pin 10
 // Sparkfun SD shield: pin 8
 // MKRZero SD: SDCARD_SS_PIN
-const int chipSelect = 10;
+//const int chipSelect = 10;
+#define chipSelect 10 //think this uses less space than const int
+
+// Using SoftwareSerial (Arduino 1.0+) or NewSoftSerial (Arduino 0023 & prior):
+#if ARDUINO >= 100
+// On Uno: camera TX connected to pin 2, camera RX to pin 3:
+SoftwareSerial cameraconnection = SoftwareSerial(2, 3);
+// On Mega: camera TX connected to pin 69 (A15), camera RX to pin 3:
+//SoftwareSerial cameraconnection = SoftwareSerial(69, 3);
+#else
+NewSoftSerial cameraconnection = NewSoftSerial(2, 3);
+#endif
+
+Adafruit_VC0706 cam = Adafruit_VC0706(&cameraconnection);
 
 void setup() {
-  //Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
-  
-  #ifndef ESP8266
+   
+#ifndef ESP8266
   while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
 #endif
   Serial.begin(9600);
@@ -53,8 +68,10 @@ void setup() {
 
   /* Display some basic information on this sensor */
   //displaySensorDetails();
-
+  
+/* Comment out card print info for space saving
   Serial.print("\nInitializing SD card...");
+  
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
@@ -115,11 +132,118 @@ void setup() {
 
   // list all files in the card with date and size
   root.ls(LS_R | LS_DATE | LS_SIZE);
+  
+End of card info printing */
+
+  // When using hardware SPI, the SS pin MUST be set to an
+  // output (even if not connected or used).  If left as a
+  // floating input w/SPI on, this can cause lockuppage.
+#if !defined(SOFTWARE_SPI)
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  if(chipSelect != 53) pinMode(53, OUTPUT); // SS on Mega
+#else
+  if(chipSelect != 10) pinMode(10, OUTPUT); // SS on Uno, etc.
+#endif
+#endif
+
+  Serial.begin(9600);
+  Serial.println("VC0706 Camera snapshot test");
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }  
+  
+  // Try to locate the camera
+  if (cam.begin()) {
+    Serial.println("Camera Found:");
+  } else {
+    Serial.println("No camera found?");
+    return;
+  }
+  // Print out the camera version information (optional)
+  char *reply = cam.getVersion();
+  if (reply == 0) {
+    Serial.print("Failed to get version");
+  } else {
+    Serial.println("-----------------");
+    Serial.print(reply);
+    Serial.println("-----------------");
+  }
+
+  // Set the picture size - you can choose one of 640x480, 320x240 or 160x120 
+  // Remember that bigger pictures take longer to transmit!
+  
+  cam.setImageSize(VC0706_640x480);        // biggest
+  //cam.setImageSize(VC0706_320x240);        // medium
+  //cam.setImageSize(VC0706_160x120);          // small
+
+  // You can read the size back from the camera (optional, but maybe useful?)
+  uint8_t imgsize = cam.getImageSize();
+  Serial.print("Image size: ");
+  if (imgsize == VC0706_640x480) Serial.println("640x480");
+  if (imgsize == VC0706_320x240) Serial.println("320x240");
+  if (imgsize == VC0706_160x120) Serial.println("160x120");
+
+  Serial.println("Snap in 3 secs...");
+  delay(3000);
+
+  if (! cam.takePicture()) 
+    Serial.println("Failed to snap!");
+  else 
+    Serial.println("Picture taken!");
+  
+  // Create an image with the name IMAGExx.JPG
+  char filename[13];
+  strcpy(filename, "IMAGE00.JPG");
+  for (int i = 0; i < 100; i++) {
+    filename[5] = '0' + i/10;
+    filename[6] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+  
+  // Open the file for writing
+  File imgFile = SD.open(filename, FILE_WRITE);
+
+  // Get the size of the image (frame) taken  
+  uint16_t jpglen = cam.frameLength();
+  Serial.print("Storing ");
+  Serial.print(jpglen, DEC);
+  Serial.print(" byte image.");
+
+  int32_t time = millis();
+  pinMode(8, OUTPUT);
+  // Read all the data up to # bytes!
+  byte wCount = 0; // For counting # of writes
+  while (jpglen > 0) {
+    // read 32 bytes at a time;
+    uint8_t *buffer;
+    uint8_t bytesToRead = min(32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
+    buffer = cam.readPicture(bytesToRead);
+    imgFile.write(buffer, bytesToRead);
+    if(++wCount >= 64) { // Every 2K, give a little feedback so it doesn't appear locked up
+      Serial.print('.');
+      wCount = 0;
+    }
+    //Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
+    jpglen -= bytesToRead;
+  }
+  imgFile.close();
+
+  time = millis() - time;
+  Serial.println("done!");
+  Serial.print(time); Serial.println(" ms elapsed");
+  
 }
 
 void loop() { 
   ultrasonic();
-  //accelerometer();
+  accelerometer();
 }
 
 void accelerometer(){
@@ -144,7 +268,7 @@ void accelerometer(){
 }
 void ultrasonic(){
   for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
-    delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    delay(29); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     Serial.print(i);
     Serial.print("=");
     Serial.print(sonar[i].ping_cm());
@@ -152,4 +276,5 @@ void ultrasonic(){
   }
   Serial.println();
 }
+
 
